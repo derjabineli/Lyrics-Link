1db9b52a4146a38234c66bfa91fbceb09e91f289
Eli Derjabin <69370608+derjabineli@users.noreply.github.com>
2024-01-30 14:10:02 -0800

    added navbar

---
+++ client/src/components/NavBar.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./navbar.css";
+
+function NavBar() {
+  return (
+    <nav>
+      <a href="/">LyricsLink.</a>
+      <ul>
+        <li href="/">Login</li>
+        <li href="/">Sign Up</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default NavBar;
