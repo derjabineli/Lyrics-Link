89732c455ec600c6ccaf1e0b6fd03ae6d7dea47c
Eli Derjabin <69370608+derjabineli@users.noreply.github.com>
2024-02-01 10:44:12 -0800

    created login component

--- client/src/components/LoginModal.js
+++ client/src/components/LoginModal.js
@@ -1,18 +1,17 @@
 import React from "react";
 import "./loginModal.css";
-import { useEffect } from "react";
 
 function loginModal() {
-  const handleLogin = () => {
-    console.log("Log in");
-  };
+  const baseURL = process.env.REACT_APP_BASEURL;
+  const clientID = process.env.REACT_APP_CLIENTID;
+  const link = `https://api.planningcenteronline.com/oauth/authorize?client_id=${clientID}&redirect_uri=${baseURL}&response_type=code&scope=services people`;
 
   return (
     <div className="loginModal">
       <h2>Log In with Planning Center</h2>
-      <button className="loginButton ctaBtn" onClick={handleLogin}>
-        Login
-      </button>
+      <a href={link} target="_blank" rel="noreferrer">
+        <button className="loginButton ctaBtn">Login</button>
+      </a>
     </div>
   );
 }
