1a2a47214c82117bcfaa39a8dd839abd40bd505a
Eli Derjabin <69370608+derjabineli@users.noreply.github.com>
2024-02-02 16:35:48 -0800

    added routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,38 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./routes/Login";
+import DashBoard from "./routes/DashBoard";
 
 function App() {
-  return <div className="App"></div>;
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    fetch("/api/user")
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.data != null) {
+          setLoggedIn(true);
+          setUser(data.data);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
+
+  console.log(loggedIn);
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          {!loggedIn && <Route path="/login" element={<Login />} />}
+          {loggedIn && <Route path="/" element={<DashBoard user={user} />} />}
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
