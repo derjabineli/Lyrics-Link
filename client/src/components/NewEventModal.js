f897e68351b7afff1c808bdc031432e10b17be0d
Eli Derjabin <69370608+derjabineli@users.noreply.github.com>
2024-02-09 13:39:11 -0800

    added create event modal component

---
+++ client/src/components/NewEventModal.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./NewEventModal.css";
+import Songs from "./Songs.js";
+
+const NewEventModal = ({ open, onClose }) => {
+  if (!open) return null;
+
+  return (
+    <div className="overlay" onClick={onClose}>
+      <div
+        className="modal_container"
+        onClick={(e) => {
+          e.stopPropagation();
+        }}
+      >
+        <form>
+          <h1>Create New Event</h1>
+          <hr />
+          <h2>Event Name</h2>
+          <input
+            className="event_name"
+            type="text"
+            name="name"
+            id="name"
+            required
+          />
+          <h2>Date</h2>
+          <input type="date" id="date" />
+          <Songs />
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default NewEventModal;
