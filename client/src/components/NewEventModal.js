b77e071bc32398b3b398fc75f17763cbdfe485c2
Eli Derjabin <69370608+derjabineli@users.noreply.github.com>
2024-02-12 15:01:20 -0800

    added state functionality to newEventModal

--- client/src/components/NewEventModal.js
+++ client/src/components/NewEventModal.js
@@ -3,6 +3,10 @@ import "./NewEventModal.css";
 import Songs from "./Songs.js";
 
 const NewEventModal = ({ open, onClose }) => {
+  const [name, setName] = useState("");
+  const [date, setDate] = useState("");
+  const [songs, setSongs] = useState([]);
+
   if (!open) return null;
 
   return (
@@ -22,11 +26,21 @@ const NewEventModal = ({ open, onClose }) => {
             type="text"
             name="name"
             id="name"
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
             required
           />
           <h2>Date</h2>
-          <input type="date" id="date" />
-          <Songs />
+          <input
+            type="date"
+            id="date"
+            onChange={(e) => {
+              setDate(e.target.value);
+            }}
+          />
+          <Songs setSongs={setSongs} />
           <button className="ctaBtn create_event_btn">Create</button>
         </form>
       </div>
