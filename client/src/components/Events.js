eb088c165cb72ed266028b1fb009a039533f7fcd
Eli Derjabin <69370608+derjabineli@users.noreply.github.com>
2024-02-12 14:59:30 -0800

    added createEvent Modal to events component

--- client/src/components/Events.js
+++ client/src/components/Events.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
 import "./Events.css";
 import Event from "./Event";
+import NewEventModal from "./NewEventModal";
 
 function Events() {
   const [events, setEvents] = useState([]);
+  const [showModal, setShowModal] = useState(false);
 
   useEffect(() => {
     fetch("/api/events")
@@ -18,21 +20,29 @@ function Events() {
   }, [events]);
 
   return (
-    <div className="container">
-      <div className="events_header">
-        <h1>Events</h1>
-        <button className="ctaBtn events_button">Add New Event</button>
+    <>
+      <NewEventModal open={showModal} onClose={() => setShowModal(false)} />
+      <div className="container">
+        <div className="events_header">
+          <h1>Events</h1>
+          <button
+            className="ctaBtn events_button"
+            onClick={() => setShowModal(true)}
+          >
+            Add New Event
+          </button>
+        </div>
+        <div className="events">
+          {events.map((service) => (
+            <Event
+              name={service.event_type}
+              date={service.event_date.substring(0, 10)}
+              songCount={service.songs.length}
+            />
+          ))}
+        </div>
       </div>
-      <div className="events">
-        {events.map((service) => (
-          <Event
-            name={service.event_type}
-            date={service.event_date.substring(0, 10)}
-            songCount={service.songs.length}
-          />
-        ))}
-      </div>
-    </div>
+    </>
   );
 }
 
