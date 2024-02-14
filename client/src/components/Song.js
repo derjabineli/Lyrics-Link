d2cd47a4893c972cf37798e75115f253032faa05
Eli Derjabin <69370608+derjabineli@users.noreply.github.com>
2024-02-13 17:59:02 -0800

    added function to add songs to new event

--- client/src/components/Song.js
+++ client/src/components/Song.js
@@ -1,11 +1,17 @@
 import React, { useEffect, useState } from "react";
 import "./Song.css";
 
-function Song({ id, title, author, link }) {
+function Song({ id, title, author, link, setSongs }) {
+  const addSongToEvent = () => {
+    setSongs((prevSongs) => [...prevSongs, id]);
+  };
+
   return (
-    <div className="song">
-      <p className="song_title">{title}</p>
-      <p className="song_author">{author}</p>
+    <div onClick={addSongToEvent}>
+      <div className="song">
+        <p className="song_title">{title}</p>
+        <p className="song_author">{author}</p>
+      </div>
     </div>
   );
 }
