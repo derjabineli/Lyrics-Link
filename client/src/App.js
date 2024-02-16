846f211dc7bc7241fb7a59e295569677abe590bf
Eli Derjabin <69370608+derjabineli@users.noreply.github.com>
2024-02-16 12:49:50 -0800

    removed context provider from app.js file

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { UserContextProvider } from "./context/UserContext";
+import { UserContext, UserContextProvider } from "./context/UserContext";
 import Login from "./routes/Login";
 import DashBoard from "./routes/DashBoard";
 import EventEdit from "./routes/EventEdit";
@@ -9,7 +9,7 @@ import Live from "./routes/Live";
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(false);
-  const [user, setUser] = useState({});
+  const { user, setUser } = useContext(UserContext);
 
   useEffect(() => {
     fetch("/api/user")
@@ -26,23 +26,19 @@ function App() {
   }, []);
 
   console.log(loggedIn);
+
   return (
     <div className="App">
-      <UserContextProvider>
-        <Router>
-          <Routes>
-            {!loggedIn && <Route path="/" element={<Login />} />}
-            {loggedIn && <Route path="/" element={<DashBoard user={user} />} />}
-            {loggedIn && (
-              <Route
-                path="/event/:id/update"
-                element={<EventEdit user={user} />}
-              />
-            )}
-            <Route path="/live/:id" element={<Live />} />
-          </Routes>
-        </Router>
-      </UserContextProvider>
+      <Router>
+        <Routes>
+          {!loggedIn && <Route path="/" element={<Login />} />}
+          {loggedIn && <Route path="/" element={<DashBoard />} />}
+          {loggedIn && (
+            <Route path="/event/:id/update" element={<EventEdit />} />
+          )}
+          <Route path="/live/:id" element={<Live />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
