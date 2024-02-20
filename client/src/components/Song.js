0d9e5a9ed21c5ee0ec42a8b653b4b49d5f44ed13
Eli Derjabin <69370608+derjabineli@users.noreply.github.com>
2024-02-20 15:39:03 -0800

    add song to database function when user clicks on song

--- client/src/components/Song.js
+++ client/src/components/Song.js
@@ -4,8 +4,36 @@ import "./Song.css";
 function Song({ id, title, author, link, setSongs }) {
   useEffect(() => {});
 
+  const addSongToDB = async (songId) => {
+    const res = await fetch(`/api/song/?id=${songId}`);
+    const data = await res.json();
+    console.log(data);
+
+    const postData = {
+      id: songId,
+      name: data.title,
+      lyrics: data.data.attributes.lyrics,
+      chord_chart: data.data.attributes.chord_chart,
+      chord_chart_key: data.data.attributes.chord_chart_key,
+    };
+
+    console.log(postData);
+
+    const postRequest = await fetch("/api/song", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "same-origin",
+      body: JSON.stringify(postData),
+    });
+
+    console.log(postRequest);
+  };
+
   const addSongToEvent = () => {
     setSongs((prevSongs) => [...prevSongs, id]);
+    addSongToDB(id);
   };
 
   return (
