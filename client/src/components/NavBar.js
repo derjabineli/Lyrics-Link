8ea2ea5587b3000255789567f71d5eeda91e5498
Eli Derjabin <69370608+derjabineli@users.noreply.github.com>
2024-02-21 13:08:34 -0800

    removed log in and sign up from Login nav

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -5,10 +5,6 @@ function NavBar() {
   return (
     <nav>
       <a href="/">LyricsLink.</a>
-      <ul>
-        <li href="/">Login</li>
-        <li href="/">Sign Up</li>
-      </ul>
     </nav>
   );
 }
