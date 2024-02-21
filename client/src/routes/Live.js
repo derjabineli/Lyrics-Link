2a0274003f57c1a10913ac683b41de1edec92739
Eli Derjabin <69370608+derjabineli@users.noreply.github.com>
2024-02-20 21:42:47 -0800

    added live lyric view page

---
+++ client/src/routes/Live.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Lyrics from "../components/Lyrics";
+import "./Live.css";
+
+const Live = () => {
+  const params = useParams();
+  const event_id = params.id;
+  const [songs, setSongs] = useState([]);
+
+  useEffect(() => {
+    const getEvent = () => {
+      fetch(`/api/event/?id=${event_id}`)
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data.rows[0].songs);
+          setSongs(data.rows[0].songs);
+        });
+    };
+    getEvent();
+  }, []);
+
+  return (
+    <div className="live">
+      {songs ? (
+        songs.map((songId) => <Lyrics id={songId} />)
+      ) : (
+        <h1>No results</h1>
+      )}
+    </div>
+  );
+};
+
+export default Live;
