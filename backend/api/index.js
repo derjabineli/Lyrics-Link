a04b75467bb3bef36eec22600dd5b951dd3ee499
Eli Derjabin <69370608+derjabineli@users.noreply.github.com>
2024-02-23 14:52:27 -0800

    remove samesite

--- backend/api/index.js
+++ backend/api/index.js
@@ -23,7 +23,7 @@ const pgSession = postgresSession(expressSession);
 const cookieSettings = {
   secure: true,
   httpOnly: false,
-  sameSite: "None",
+  //   sameSite: "None",
   maxAge: 1000 * 60 * 60 * 24, // 1 day
   path: "/api",
 };
