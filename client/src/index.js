8759f70aceba23d3874e996d6591b896652fb37e
Eli Derjabin <69370608+derjabineli@users.noreply.github.com>
2024-02-22 22:24:15 -0800

    removed strict mode

--- client/src/index.js
+++ client/src/index.js
@@ -6,9 +6,7 @@ import { UserContextProvider } from "./context/UserContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <UserContextProvider>
-      <App />
-    </UserContextProvider>
-  </React.StrictMode>
+  <UserContextProvider>
+    <App />
+  </UserContextProvider>
 );
