d977caf36e8c821f51e7243769ebe6505014dcbd
Eli Derjabin <69370608+derjabineli@users.noreply.github.com>
2024-02-23 15:06:31 -0800

    removed secure

--- backend/api/index.js
+++ backend/api/index.js
@@ -31,7 +31,6 @@ app.use(
     resave: false,
     saveUninitialized: false,
     cookie: {
-      secure: true,
       httpOnly: false,
       sameSite: "lax",
       maxAge: 1000 * 60 * 60 * 24, // 1 day
