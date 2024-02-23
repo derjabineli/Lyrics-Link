0a330f330e7f917b88ad337173373c07cfdeb96d
Eli Derjabin <69370608+derjabineli@users.noreply.github.com>
2024-02-23 10:59:43 -0800

    console loggin cookie

--- backend/api/index.js
+++ backend/api/index.js
@@ -99,7 +99,7 @@ app.get("/api/callback", async (req, res) => {
 
 app.get("/api/user", async (req, res) => {
   try {
-    console.log(req.session);
+    console.log(req.headers.cookie);
     const user = await getUser(req.session.access_token);
     res.send(user);
   } catch (err) {
