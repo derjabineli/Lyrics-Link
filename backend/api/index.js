0e969fd6b5da3943b1c71510301346817951daf4
Eli Derjabin <69370608+derjabineli@users.noreply.github.com>
2024-02-23 18:10:05 -0800

    same site none

--- backend/api/index.js
+++ backend/api/index.js
@@ -33,7 +33,7 @@ app.use(
     cookie: {
       secure: true,
       httpOnly: false,
-      sameSite: "lax",
+      sameSite: "None",
       maxAge: 1000 * 60 * 60 * 24, // 1 day
       path: "/api",
     },
