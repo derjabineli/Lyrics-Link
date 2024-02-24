91f5ffc43b300ae658283b5b77e57400790be8e8
Eli Derjabin <69370608+derjabineli@users.noreply.github.com>
2024-02-23 18:52:16 -0800

    allow origin frontendurl

--- backend/api/index.js
+++ backend/api/index.js
@@ -52,7 +52,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", FRONTENDURL);
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
