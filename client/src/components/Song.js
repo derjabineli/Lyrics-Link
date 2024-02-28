67f0fa9c0e288517a6a2528d2cffe470b560bd50
Eli Derjabin <69370608+derjabineli@users.noreply.github.com>
2024-02-27 16:37:46 -0800

    log response

--- client/src/components/Song.js
+++ client/src/components/Song.js
@@ -5,33 +5,28 @@ function Song({ id, title, author, link, setSongs }) {
   useEffect(() => {});
 
   const addSongToDB = async (songId) => {
-    const res = await fetch(
-      process.env.REACT_APP_APIURL + `/api/song/?id=${songId}`,
-      {
-        credentials: "include",
-      }
-    );
-    const data = await res.json();
+    fetch(process.env.REACT_APP_APIURL + `/api/song/?id=${songId}`, {
+      credentials: "include",
+    })
+      .then((res) => res.json())
+      .then(async (data) => {
+        const postData = {
+          id: songId,
+          name: data.title,
+          lyrics: data.data.attributes.lyrics,
+          chord_chart: data.data.attributes.chord_chart,
+          chord_chart_key: data.data.attributes.chord_chart_key,
+        };
 
-    const postData = {
-      id: songId,
-      name: data.title,
-      lyrics: data.data.attributes.lyrics,
-      chord_chart: data.data.attributes.chord_chart,
-      chord_chart_key: data.data.attributes.chord_chart_key,
-    };
-
-    const postRequest = await fetch(
-      process.env.REACT_APP_APIURL + "/api/song",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        credentials: "include",
-        body: JSON.stringify(postData),
-      }
-    );
+        fetch(process.env.REACT_APP_APIURL + "/api/song", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          credentials: "include",
+          body: JSON.stringify(postData),
+        }).then((res) => console.log(res));
+      });
   };
 
   const addSongToEvent = () => {
